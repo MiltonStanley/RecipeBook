8632747417440cd3123c172a2db6ed874eac7925
MiltonStanley <milton.stanley@gmail.com>
2012-11-24 01:24:51 -0600

    Resetting and starting over

--- structures.rb
+++ structures.rb
@@ -26,28 +26,9 @@ class Recipe
     ingredient_number = 1
     done = false
     until done
-      puts 'Top of loop'
-      new_ingredient = Ingredient.new(ingredient_number, done)
-      puts 'back in loop'
-      puts "done is now " + done.to_s
-      puts "new_ingredient is now" + new_ingredient.to_s
-      ingredient_number += 1
-      @ingredients.push new_ingredient unless done
+      input = gets.chomp
+      done = true if input == ""
     end
-=begin
-    puts "Directions (return on empty line to end)"
-    @directions = Array.new
-    direction_number = 0
-    done = false
-    until done
-      direction_number += 1
-      print "Direction #{direction_number}: "
-      _direction = gets.chomp
-      done = true if _direction == ""
-      @directions.push _direction unless done
-    end
-=end
-
     print "Serving size: "
     @serving_size = gets.chomp
   end
@@ -72,13 +53,7 @@ class Ingredient
   attr_accessor :quantity, :name #:price_list
 
   def initialize(ingredient_number, done)
-    print "Ingredient #{ingredient_number}: "
-    ingredient = gets.chomp
-    _quantity, _name = ingredient.split('|')
-    done = true unless _quantity.nil? || _name.nil?
-    @quantity = _quantity.strip unless _quantity.nil?
-    @name = _name.strip unless _name.nil?
-    puts "done is " + done.to_s
+
   end
 
   def display
