5f095646e48a0de75516f3aa0bb373b2292f8e7e
MiltonStanley <milton.stanley@gmail.com>
2012-11-24 15:25:16 -0600

    Finished yaml dumping

--- recipebook.rb
+++ recipebook.rb
@@ -8,18 +8,24 @@ require 'yaml'
 require './structures'
 system "clear"
 
+recipe_array = YAML::load(File.open('myrecipes.yaml'))
+
 book = Recipe_Book.new
+recipe_array.each { |recipe| book.push recipe }
 
 while true
-  puts "1) Exit 2) New Recipe 3) List All Recipes 4) Find a recipe"
+  puts "1) New Recipe 2) List All Recipes 3) Find a recipe 4) Save and exit"
   print "--> "
   command = gets.chomp
   case command
-    when "1" then break
-    when "2" then new_recipe = Recipe.new
+    when "1" then new_recipe = Recipe.new
                   book.push new_recipe
-    when "3" then book.list
-    when "4" then book.find
+    when "2" then book.list
+    when "3" then book.find
+    when "4" then myrecipes = File.open('myrecipes.yaml','w')
+                  myrecipes.puts book.to_yaml
+                  myrecipes.close
+                  break
     else puts "Invalid command!"
   end
 end
