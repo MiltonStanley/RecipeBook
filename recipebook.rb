934985fa3409803b5936c0e9d5158ef29f6874ed
MiltonStanley <milton.stanley@gmail.com>
2012-11-23 23:08:35 -0600

    Making progress; return values bad

--- recipebook.rb
+++ recipebook.rb
@@ -6,16 +6,15 @@ stay = true
 book = Recipe_Book.new
 
 while stay
-  puts "1) Exit 2) New Recipe 3) List All Recipes 4) Find a recipe 5) Edit Pantry"
+  puts "1) Exit 2) New Recipe 3) List All Recipes 4) Find a recipe"
   print "Job to do: "
   command = gets.chomp
   case command
     when "1" then stay = false
     when "2" then new_recipe = Recipe.new
-        book.recipes.push new_recipe
+        book.push new_recipe
     when "3" then book.list
     when "4" then book.find
-    when "5" then "edit pantry"
     else puts "Invalid command!"
   end
 end
--- structures.rb
+++ structures.rb
@@ -1,20 +1,14 @@
 class Recipe_Book < Array
-  attr_accessor :recipes
-
-  def initialize
-    @recipes = Array.new
-  end
 
   def list
-    recipe_number = 1
-    @recipes.each { |recipe| puts "#{recipe_number}: #{recipe.name}" }
+    self.each_index { |index| puts "#{index+1}: #{self[index].name}" }
   end
 
   def find
     self.list
     print "Show recipe #: "
     recipe = gets.chomp.to_i
-    self[recipe].display
+    self[recipe-1].display
   end
 
 end
@@ -29,14 +23,27 @@ class Recipe
 
     puts "Ingredients (return on empty line to end)"
     @ingredients = Array.new
-    ingredient_number = 0
+    ingredient_number = 1
     done = false
     until done
+      'Top of loop'
+      puts done.to_s
+      new_ingredient, done = Ingredient.new(ingredient_number)
+      puts new_ingredient
+      puts "done is now " + done.to_s
+=begin
+      input = gets.chomp
+      done = true if input == ""
+      puts done.to_s
+      puts "adding ingredient now"
+
+      puts "done is " + done.to_s
+      new_ingredient, done = Ingredient.new(ingredient_number)
+      puts "done is now " + done.to_s
+      puts "new_ingredient is now" + new_ingredient.to_s
       ingredient_number += 1
-      print "Ingredient #{ingredient_number}: "
-      _ingredient = gets.chomp
-      done = true if _ingredient == ""
-      @ingredients.push _ingredient
+      @ingredients.push new_ingredient unless done
+=end
     end
 
     puts "Directions (return on empty line to end)"
@@ -48,7 +55,7 @@ class Recipe
       print "Direction #{direction_number}: "
       _direction = gets.chomp
       done = true if _direction == ""
-      @directions.push _direction
+      @directions.push _direction unless done
     end
 
     print "Serving size: "
@@ -56,9 +63,13 @@ class Recipe
   end
 
   def display
-    puts :name
-    :ingredients.each { |ingredient| ingredient.display}
-
+    puts self.name
+    
+    #  index.to_s is index; "#{index}" is the same
+    
+    @ingredients.each_index { |index| puts "#{index+1}: #{@ingredients[index].name}" }
+    puts "----"
+    @directions.each_index { |index| puts @directions[index].display }
   end
 
   def edit
@@ -67,21 +78,27 @@ class Recipe
 
 end
 
-class Pantry
-  attr_accessor :ingredients
-
-  def display
-    puts 'display ingredients'
-  end
-
-  def edit
-    puts 'edit existing ingredients'
-  end
-
-end
-
 class Ingredient
-  attr_accessor :quantity, :name, :price_list
+  attr_accessor :quantity, :name #:price_list
+
+  def initialize(ingredient_number)
+    valid = false
+    input = gets.chomp
+    valid = true if input == ""
+    puts "valid is " + valid.to_s
+    puts "self is" + self.to_s
+=begin
+    print "Ingredient #{ingredient_number}: "
+    ingredient = gets.chomp
+    _quantity, _name = ingredient.split('|')
+   # valid = true unless _quantity.nil? || _name.nil?
+    @quantity = _quantity.strip unless _quantity.nil?
+    @name = _name.strip unless _name.nil?
+    puts "valid is " + valid.to_s
+=end
+    ret = [self, valid]
+    return ret
+  end
 
   def display
     puts "#{:quantity} - #{:name}"
@@ -93,7 +110,7 @@ class Direction
   attr_accessor :number, :text, :ingredients
 
   def display
-    puts ""
+    puts "display direction"
   end
 
 end
