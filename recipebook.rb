abea705f06e3a465bd742417028ee883717a78e8
MiltonStanley <milton.stanley@gmail.com>
2012-11-23 21:19:56 -0600

    Scaffolding interface

--- recipebook.rb
+++ recipebook.rb
@@ -1,15 +1,21 @@
 class Recipe
-
-  def initialize
-
-  end
+  attr_accessor :name, :ingredients, :directions, :serving_size
 
   def print
-
+    # cycle through attributes, printing each
   end
 
   def edit
+    # make changes
+  end
 
+end
+
+class Pantry
+  attr_accessor :ingredients
+
+  def print
+    # print ingredients
   end
 
 end
@@ -17,10 +23,32 @@ end
 class Ingredient
   attr_accessor :quantity, :name, :price_list
 
+  def print
+    #nicely formatted printing
+  end
 
 end
 
 class Direction
   attr_accessor :number, :text, :ingredients
 
+  def print
+    # nicely formatted printing
+  end
+
+end
+
+stay = true
+
+while stay
+  puts "1) Exit 2) New Recipe 3) Show Recipes 4) Edit Pantry"
+  print "Job to do: "
+  command = gets.chomp
+  case command
+    when "1" then stay = false
+    when "2" then puts "new recipe"
+    when "3" then puts "show recipes"
+    when "4" then puts "edit pantry"
+    else puts "Invalid command!"
+  end
 end
