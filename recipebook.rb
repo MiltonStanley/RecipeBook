10ca079a81a39b78084e55a48e3055fe347ef324
MiltonStanley <milton.stanley@gmail.com>
2012-11-23 21:13:10 -0600

    Created project

---
+++ recipebook.rb
@@ -0,0 +1,26 @@
+class Recipe
+
+  def initialize
+
+  end
+
+  def print
+
+  end
+
+  def edit
+
+  end
+
+end
+
+class Ingredient
+  attr_accessor :quantity, :name, :price_list
+
+
+end
+
+class Direction
+  attr_accessor :number, :text, :ingredients
+
+end
